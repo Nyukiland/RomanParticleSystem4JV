498a3dbf8ce85f1735ac0eaf727c8690738a4598
Nyukiland <roman211113@gmail.com>
2025-05-23 14:52:18 +0200

    [UPD] Particle blink

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,45 @@
 
 int main()
 {
+    struct Particle
+    {
+        glm::vec2 Pos = glm::vec2(
+            utils::rand(-gl::window_aspect_ratio(), gl::window_aspect_ratio()),
+            utils::rand(-1,1)
+        );
+    };
+    
+
     gl::init("Particules!");
     gl::maximize_window();
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    // TODO: create an array of particles
+
+    float time = 0;
+
+    int particleCount = 100;
+
+    std::vector<Particle> particles;
+    particles.reserve(particleCount);
+    for (int i = 0; i < particleCount; ++i)
+    {
+        particles.emplace_back();
+    }
 
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        if (time < 1) time += gl::time_in_seconds() / 10;
+        else time -= gl::time_in_seconds() / 10;
+
+        for (Particle particle : particles)
+        {
+            
+            utils::draw_disk(particle.Pos, time/100, glm::vec4(1,1,1,1));
+        }
 
         // TODO update particles
         // TODO render particles
