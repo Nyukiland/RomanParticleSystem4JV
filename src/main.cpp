22a560bb1de5fc50d146c9de8b99083670e0037e
Nyukiland <roman211113@gmail.com>
2025-07-11 16:48:30 +0200

    [UPD] Grid

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,9 @@
 #include "utils.hpp"
 #include <numbers>
 
-struct ParticleRectangle
+struct Particle
 {
-     glm::vec2 Pos = glm::vec2(
-        utils::rand(-gl::window_aspect_ratio() + 0.1, -gl::window_aspect_ratio() + 0.6),
-        utils::rand(-0.9,-0.2)
-    );
-};
-
-struct ParticleParallelogram
-{
-    float y = utils::rand(-0.9,-0.2);
-
-    glm::vec2 Pos = glm::vec2(
-        utils::rand(gl::window_aspect_ratio() - 0.6 + y, gl::window_aspect_ratio() - 0.1 + y),
-        y
-    );
-};
-
-struct ParticleCircle
-{
-    //pi * r^2 = var_uniforme
-    float var = utils::rand(0, 0.4);
-    float r = glm::sqrt(var/std::numbers::pi);
-    
-    float angle = utils::rand(0, 360);
-
-    glm::vec2 Pos = glm::vec2(glm::sin(angle), glm::cos(angle)) * r;
+     glm::vec2 Pos = glm::vec2(0,0);
 };
 
 int main()
@@ -38,44 +14,34 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    int particleCount = 1000;
-    std::vector<ParticleRectangle> particlesRectangle;
-    particlesRectangle.reserve(particleCount);
-    for (int i = 0; i < particleCount; ++i)
+    float spacing = 0.1;
+
+    float xCount = (gl::window_aspect_ratio()*2)/spacing;
+    float yCount = 2/spacing;
+
+    int index = 0;
+
+    int particleCount = xCount * yCount;
+    std::vector<Particle> particles;
+    particles.reserve(particleCount);
+    for (int x = 0; x < xCount; ++x)
     {
-        particlesRectangle.emplace_back();
+        for (int y = 0; y < yCount; ++y)
+        {
+            particles.emplace_back();
+            
+            particles[index].Pos = glm::vec2(-gl::window_aspect_ratio() + spacing*x, -1 + spacing*y);
+            index++;
+        }
     }
 
-    std::vector<ParticleParallelogram> particlesParallelogram;
-    particlesParallelogram.reserve(particleCount);
-    for (int i = 0; i < particleCount; ++i)
-    {
-        particlesParallelogram.emplace_back();
-    }
-
-    std::vector<ParticleCircle> particlesCircle;
-    particlesCircle.reserve(particleCount);
-    for (int i = 0; i < particleCount; ++i)
-    {
-        particlesCircle.emplace_back();
-    }
 
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        for (ParticleRectangle& particle : particlesRectangle)
-        {
-            utils::draw_disk(particle.Pos, 0.01, glm::vec4(1,1,1,1));
-        }
-
-        for (ParticleParallelogram& particle : particlesParallelogram)
-        {
-            utils::draw_disk(particle.Pos, 0.01, glm::vec4(1,1,1,1));
-        }
-
-        for (ParticleCircle& particle : particlesCircle)
+        for (Particle& particle : particles)
         {
             utils::draw_disk(particle.Pos, 0.01, glm::vec4(1,1,1,1));
         }
