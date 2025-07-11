c520ead7c40258c4059bdfa9d2d7ca59fb796bce
Nyukiland <roman211113@gmail.com>
2025-07-11 14:51:09 +0200

    [UPD] Rectangle and Parallelogram

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include "opengl-framework/opengl-framework.hpp"
 #include "utils.hpp"
 
+struct ParticleRectangle
+{
+     glm::vec2 Pos = glm::vec2(
+        utils::rand(-gl::window_aspect_ratio() + 0.1, -gl::window_aspect_ratio() + 0.6),
+        utils::rand(-0.9,-0.2)
+    );
+};
+
+struct ParticleParallelogram
+{
+    float y = utils::rand(-0.9,-0.2);
+
+    glm::vec2 Pos = glm::vec2(
+        utils::rand(gl::window_aspect_ratio() - 0.6 + y, gl::window_aspect_ratio() - 0.1 + y),
+        y
+    );
+};
+
 int main()
 {
     gl::init("Distribution!");
@@ -8,14 +26,34 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
+    int particleCount = 100;
+    std::vector<ParticleRectangle> particlesRectangle;
+    particlesRectangle.reserve(particleCount);
+    for (int i = 0; i < particleCount; ++i)
+    {
+        particlesRectangle.emplace_back();
+    }
 
-
+    std::vector<ParticleParallelogram> particlesParallelogram;
+    particlesParallelogram.reserve(particleCount);
+    for (int i = 0; i < particleCount; ++i)
+    {
+        particlesParallelogram.emplace_back();
+    }
 
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        
+        for (ParticleRectangle& particle : particlesRectangle)
+        {
+            utils::draw_disk(particle.Pos, 0.01, glm::vec4(1,1,1,1));
+        }
+
+        for (ParticleParallelogram& particle : particlesParallelogram)
+        {
+            utils::draw_disk(particle.Pos, 0.01, glm::vec4(1,1,1,1));
+        }
     }
 }
