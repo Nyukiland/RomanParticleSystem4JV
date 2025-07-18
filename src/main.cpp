9d46ea781265f9d7017bdb3b709c0a914d3255b6
Nyukiland <roman211113@gmail.com>
2025-07-18 10:36:31 +0200

    [UPDATE] Bezier De Casteljau

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,36 @@ void draw_parametric(std::function<glm::vec2(float)> const& parametric)
     }
 }
 
+glm::vec2 lerp(const glm::vec2 a, const glm::vec2 b, float t) {
+    return (1.0f - t) * a + t * b;
+}
+
+glm::vec2 bezier1(const glm::vec2 p0, const glm::vec2 p1, float t)
+{
+    return lerp(p0, p1, t);
+}
+
+glm::vec2 bezier2(const glm::vec2 p0, const glm::vec2 p1, const glm::vec2 p2, float t)
+{
+    glm::vec2 A = lerp(p0, p1, t);
+    glm::vec2 B = lerp(p1, p2, t);
+
+    return lerp(A, B, t);
+}
+
+
+glm::vec2 bezier3(const glm::vec2 p0, const glm::vec2 p1, const glm::vec2 p2, const glm::vec2 p3, float t)
+{
+    glm::vec2 A = lerp(p0, p1, t);
+    glm::vec2 B = lerp(p1, p2, t);
+    glm::vec2 C = lerp(p2, p3, t);
+
+    glm::vec2 D = lerp(A, B, t);
+    glm::vec2 E = lerp(B, C, t);
+
+    return lerp(D, E, t);
+}
+
 
 int main()
 {
@@ -32,15 +62,21 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         draw_parametric([](float t) {
-            float angle = t * 2 * std::numbers::pi;
-            return glm::vec2(std::cos(angle)/2, std::sin(angle)/2);
+            return bezier1({-0.5f, -0.5f}, {-0.5f, 0.5f}, t);
+        });
+
+         draw_parametric([](float t) {
+            return bezier2({-0.3f, 0.5f}, gl::mouse_position(), {0.8f, 0.5f}, t);
+        });
+
+         draw_parametric([](float t) {
+            return bezier3({-0.3f, -0.3f}, {0, 0}, gl::mouse_position(), {0.8f, -0.3f}, t);
         });
     }
 }
